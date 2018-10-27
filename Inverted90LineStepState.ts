f72c6c8e9d8d2c31024a0c14b6679e55bc520472
Anwesh43 <anweshthecool0@gmail.com>
2018-10-27 12:40:22 +0530

    adding ILNode class

--- Inverted90LineStepState.ts
+++ Inverted90LineStepState.ts
@@ -72,3 +72,61 @@ class Animator {
         }
     }
 }
+
+class ILNode {
+    prev : ILNode
+    next : ILNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new ILNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / 3
+        context.lineWidth = Math.min(w, h) / 60
+        context.lineCap = 'round'
+        context.strokeStyle = 'teal'
+        context.save()
+        context.translate(gap * this.i + gap, h/2)
+        for (var j = 0; j < 2; j++) {
+            const sc : number = Math.min(0.5, Math.max(0, this.state.scale - 0.5 * j))
+            context.save()
+            context.rotate(Math.PI/2 * j + Math.PI/2 * sc * (2 * j - 1))
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(size, 0)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number,cb : Function) : ILNode {
+        var curr : ILNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
