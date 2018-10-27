ed6d2524276c2afbd53ee1592d1b6e3694ae422a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-27 12:45:49 +0530

    renmaing FileName

---
+++ Inverted90LineStepStage.ts
@@ -0,0 +1,165 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+
+class Inverted90LineStepStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+    ils : Inverted90LineStep = new Inverted90LineStep()
+    animator : Animator = new Animator()
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#BDBDBD'
+        this.context.fillRect(0, 0, w, h)
+        this.ils.draw(this.context)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+            this.ils.startUpdating(() => {
+                this.animator.start(() => {
+                    this.render()
+                    this.ils.update(() => {
+                        this.animator.stop()
+                        this.render()
+                    })
+                })
+            })
+        }
+    }
+
+    static init() {
+        const stage : Inverted90LineStepStage = new Inverted90LineStepStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
+
+class State {
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += 0.05 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
+
+class Animator {
+    animated : boolean = false
+    interval : number
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            clearInterval(this.interval)
+            this.animated = false
+        }
+    }
+}
+
+class ILNode {
+    prev : ILNode
+    next : ILNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new ILNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / 3
+        context.lineWidth = Math.min(w, h) / 60
+        context.lineCap = 'round'
+        context.strokeStyle = 'teal'
+        context.save()
+        context.translate(gap * this.i + gap, h/2)
+        for (var j = 0; j < 2; j++) {
+            const sc : number = Math.min(0.5, Math.max(0, this.state.scale - 0.5 * j))
+            context.save()
+            context.rotate(Math.PI/2 * j + Math.PI/2 * sc * (2 * j - 1))
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(size, 0)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number,cb : Function) : ILNode {
+        var curr : ILNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
+
+class Inverted90LineStep {
+    root : ILNode = new ILNode(0)
+    curr : ILNode = this.curr
+    dir : number = 1
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
